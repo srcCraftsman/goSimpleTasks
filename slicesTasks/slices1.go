d6fef2862ba39a505993e57fc00cdab06fe13df8
blacknoise <blacknoise228@gmail.com>
2024-08-01 16:19:47 +0500

    solved first slices task and added this in interface

---
+++ slicesTasks/slices1.go
@@ -0,0 +1,25 @@
+package slicesTasks
+
+//1. Создание слайса
+//Напишите программу, которая создает слайс из первых N целых чисел и выводит его.
+
+import (
+	"fmt"
+)
+
+func slice1() {
+
+	var sliceNum []int
+	var inputCount, inputNum int // inputCount = N; inputNum = numbers added in slice
+
+	fmt.Println("Set N")
+	fmt.Scan(&inputCount)
+	fmt.Println("\nEnter the numbers")
+
+	for i := 0; i < inputCount; i++ {
+		fmt.Scan(&inputNum)
+		sliceNum = append(sliceNum, inputNum)
+
+	}
+	fmt.Println("\n", sliceNum)
+}
