e2c5d31b55b5e5eeabe49594847f6b67b2fac35c
blacknoise228 <blacknoise228@gmail.com>
2024-08-05 01:33:49 +0500

    edit front

--- frontWork/front.go
+++ frontWork/front.go
@@ -10,13 +10,7 @@ import (
 func SetTasks() {
 	var val int = 99
 	for val != 0 {
-		fmt.Println(`
-Set task:
-1. Cicles
-2. Slices
-3. Cicles and Slices
-0. Stop
-`)
+		
 		fmt.Scan(&val)
 		switch {
 		case val == 1:
