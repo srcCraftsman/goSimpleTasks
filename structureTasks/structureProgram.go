3ab6c1f455048f0e7aed2d8420c217fc309f8dfb
blacknoise <blacknoise228@gmail.com>
2024-08-13 20:50:50 +0500

    micro change

--- structureTasks/structureProgram.go
+++ structureTasks/structureProgram.go
@@ -30,52 +30,52 @@ func SetStructure() {
 		case val == 1:
 
 			structure1()
-			fmt.Println("\n")
+			fmt.Printf("\n")
 
 		case val == 2:
 
 			structure2()
-			fmt.Println("\n")
+			fmt.Printf("\n")
 
 		case val == 3:
 
 			structure3()
-			fmt.Println("\n")
+			fmt.Printf("\n")
 
 		case val == 4:
 
 			structure4()
-			fmt.Println("\n")
+			fmt.Printf("\n")
 
 		case val == 5:
 
 			structure5()
-			fmt.Println("\n")
+			fmt.Printf("\n")
 
 		case val == 6:
 
 			structure6()
-			fmt.Println("\n")
+			fmt.Printf("\n")
 
 		case val == 7:
 
 			structure7()
-			fmt.Println("\n")
+			fmt.Printf("\n")
 
 		case val == 8:
 
 			structure8()
-			fmt.Println("\n")
+			fmt.Printf("\n")
 
 		case val == 9:
 
 			structure9()
-			fmt.Println("\n")
+			fmt.Printf("\n")
 
 		case val == 10:
 
 			structure10()
-			fmt.Println("\n")
+			fmt.Printf("\n")
 		}
 	}
 }
